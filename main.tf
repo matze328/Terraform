ac2ed2fe3e26724e0e7916f120e26e70c4527b80
matze328 <152269645+matze328@users.noreply.github.com>
2024-07-24 13:55:45 +0200

    up

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+resource "aws_instance" "tolle-ec2" {
+    instance_type = "t2.micro"
+    ami = "ami-0e872aee57663ae2d"
+  vpc_security_group_ids = ["sg-0e9a1cf95d737fff2"]
+  key_name = "popel123"
+}
