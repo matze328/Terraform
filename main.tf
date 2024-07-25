5ff595dd83ab39462aa5a39882b8360f2e33dc63
matze328 <152269645+matze328@users.noreply.github.com>
2024-07-25 11:57:17 +0200

    Update main.tf
    - mit Tag

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ resource "aws_instance" "tolle-ec2" {
     ami = "ami-0e872aee57663ae2d"
   vpc_security_group_ids = ["sg-0e9a1cf95d737fff2"]
   key_name = "popel123"
-}
+
+tags = {
+    Name = "tolle-ec2"       
+  }
+  }
